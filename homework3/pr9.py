c7f7219e9b4c96a182d7673daec05eb65aa546ba
Perejil2aborta <danil.topilin.95@gmail.com>
2024-10-12 12:21:20 +0500

    Update pr9.py

--- homework3/pr9.py
+++ homework3/pr9.py
@@ -1,1 +1,16 @@
+numbers = []
 
+while True:
+    user_input = input("Введите число (или 'end' для завершения): ")
+    
+    if user_input.lower() == 'end':
+        break  
+    if user_input.isdigit() or (user_input[0] == '-' and user_input[1:].isdigit()):
+        number = int(user_input)
+        numbers.append(number)
+    else:
+        print("Пожалуйста, введите корректное число или 'end' для завершения.")
+
+nechet_numbers = [num for num in numbers if num % 2 != 0]
+
+print("Нечетные числа:", nechet_numbers)
