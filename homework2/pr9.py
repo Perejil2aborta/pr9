85b35823ae4c63de7ce6de0de29420d51fd46be1
Perejil2aborta <danil.topilin.95@gmail.com>
2024-10-12 13:03:57 +0500

    Update pr9.py

--- homework2/pr9.py
+++ homework2/pr9.py
@@ -3,13 +3,12 @@ import math
 a = float(input("Введите число a: "))
 b = float(input("Введите число b: "))
 
-
 if a > b:
     a, b = b, a  
 
 a_int = math.ceil(a)
 b_int = math.floor(b)
 
-kvadrati = [i**2 for i in range(a_int, b_int + 1)]
+kvadrati = [i**2 for i in range(a_int + 1, b_int)]
 
 print("Квадраты целых чисел между", a, "и", b, ":", kvadrati)
